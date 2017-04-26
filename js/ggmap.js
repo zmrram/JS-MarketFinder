4a3099946f0781bea2acf89a5d54c2a8a74879d0
Tin Huynh <Tin Huynh>
2017-04-25 23:14:37 -0700

    add gg map to page

---
+++ js/ggmap.js
@@ -0,0 +1,46 @@
+$(function() {
+	
+	var allLatLong = [];
+	var allMarkers = [];
+	var infoWindow = null;
+	var position;
+	var userCoord;
+	var tempMarkerHolder = [];
+
+	if (navigator.geolocation) {
+
+		function error (err){
+			console.warn('ERROR(' + err.code + '): ' + err.message);
+		}
+
+		function success(position){
+			userCoord = position.coords;
+		}
+
+		navigator.geolocation.getCurrentPosition(success,error);
+	}
+	else {
+		alert('Geolocation is not supported')
+	}
+
+	var mapOptions = {
+		zoom : 5,
+		center: new google.maps.LatLng(32.09024,-100,712991),
+		panControl : false,
+		panControlOptions : {
+			position: google.maps.ControlPosition.BOTTOM_LEFT
+		},
+		zoomControl : true,
+		zoomControlOptions :{
+			style : google.maps.ZoomControlStyle.LARGE,
+			position: google.maps.ControlPosition.RIGHT_CENTER
+		},
+		scaleControl : false
+	};
+
+	infoWindow = new google.maps.InfoWindow({
+		content : ""
+	});
+
+	map = new google.maps.Map(document.getElementById('map'),mapOptions);
+});
