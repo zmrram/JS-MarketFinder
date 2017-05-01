3d377bd9dc4ab90d642d5a03ecc2ea2cb7c23c29
Tin Huynh <Tin Huynh>
2017-04-30 19:52:57 -0700

    Using farmers markets API to get data of markets

--- js/ggmap.js
+++ js/ggmap.js
@@ -48,12 +48,43 @@ $(function() {
 	google.maps.event.addListener (map, 'click', function(){
 		infoWindow.close();
 	});
-
+	
 	$('#search').click(findStore);
 	function findStore(){
 		map.setCenter({lat : userCoord.latitude, lng : userCoord.longitude});
 		map.setZoom(10);
+		var marketIds = [];
+		var market = [];
+		var maddress = [];
+		$.ajax({
+			url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat='+userCoord.latitude + "&lng=" + userCoord.longitude,
+			type: 'GET',
+			dataType: 'json',
+			success : function(data){
+				for (var i = 0; i < data.results.length; i++){
+					marketIds[i] = data.results[i].id;
+					market[i] = {marketName : data.results[i].marketname, marketAddress : ""};
+				}
+				var index = 0;
+				while (index < data.results.length){
+					$.ajax({
+						url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id='+marketIds[index],
+						type: 'GET',
+						dataType: 'json',
+						success : function (data){
+							maddress.push(data.marketdetails.Address);	
+							console.log(maddress);				
+						}
+					});	
+					index++;			
+				}
+				console.log(maddress);
+				console.log(market);
+			}
+
+		});
 		for (var i = 0; i < sampleData.data.length; i++){
+			console.log("Hello");
 			var marker = new google.maps.Marker({
 			position : sampleData.data[i].location,
 			map : map,
